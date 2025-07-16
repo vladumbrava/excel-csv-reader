07ff8081e10916eade53ce8bba7f744ad8bf5b8c
vlad.dumbrava <vladdumbrava5@gmail.com>
2025-07-16 15:02:46 +0300

    Implement endpoints in controller

---
+++ src/main/java/com/vladdumbrava/excel_csv_reader/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.vladdumbrava.excel_csv_reader.controller;
+
+import com.vladdumbrava.excel_csv_reader.dto.EmployeeDTO;
+import com.vladdumbrava.excel_csv_reader.model.Employee;
+import com.vladdumbrava.excel_csv_reader.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    public void createEmployee(@RequestBody Employee employee) {
+        employeeService.createEmployee(employee);
+    }
+
+    @GetMapping
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PutMapping("/update-employee/{id}")
+    public void updateEmployee(@PathVariable Long id, @RequestBody Employee newEmployee) {
+        employeeService.updateEmployee(id, newEmployee);
+    }
+
+    @PatchMapping("/update-employee-name/{id}")
+    public void updateEmployeeName(@PathVariable Long id, @RequestBody String name) {
+        employeeService.updateEmployeeName(id, name);
+    }
+
+}
