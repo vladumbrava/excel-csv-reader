b10e9e34d5484dc24b98aa7bc29daa1d7da637df
vlad.dumbrava <vladdumbrava5@gmail.com>
2025-07-16 14:56:24 +0300

    Implement CRUD methods in service

--- src/main/java/com/vladdumbrava/excel_csv_reader/service/EmployeeService.java
+++ src/main/java/com/vladdumbrava/excel_csv_reader/service/EmployeeService.java
@@ -1,9 +1,16 @@
 package com.vladdumbrava.excel_csv_reader.service;
 
+import com.vladdumbrava.excel_csv_reader.dto.EmployeeDTO;
+import com.vladdumbrava.excel_csv_reader.model.Employee;
 import com.vladdumbrava.excel_csv_reader.repository.EmployeeRepository;
+import com.vladdumbrava.excel_csv_reader.service.dtomapper.EmployeeDTOMapper;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +18,44 @@ import org.springframework.stereotype.Service;
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final EmployeeDTOMapper employeeDTOMapper;
 
+    public void createEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
 
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeRepository.findAll()
+                .stream()
+                .map(employeeDTOMapper)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public void updateEmployee(Long id, Employee newEmployee) {
+        employeeRepository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setAge(newEmployee.getAge());
+                    employee.setGender(newEmployee.getGender());
+                    employee.setRole(newEmployee.getRole());
+                    employee.setEmail(newEmployee.getEmail());
+                    employee.setPhoneNumber(newEmployee.getPhoneNumber());
+                    employee.setActive(newEmployee.getActive());
+                    return employeeRepository.save(employee);
+                })
+                .orElseGet(() -> employeeRepository.save(newEmployee));
+    }
+
+    public void updateEmployeeName(Long id, String newName) {
+        employeeRepository.findById(id)
+                .map(employee -> {
+                    employee.setName(newName);
+                    return employeeRepository.save(employee);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found with id: " + id));
+    }
 }
