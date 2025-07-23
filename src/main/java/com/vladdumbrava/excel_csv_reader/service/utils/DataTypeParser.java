3911a33c7e36341bd28458ee718c857e9ee86c8d
vlad.dumbrava <vladdumbrava5@gmail.com>
2025-07-23 12:05:38 +0300

    Annotate DataTypeParser with @UtilityClass

--- src/main/java/com/vladdumbrava/excel_csv_reader/service/utils/DataTypeParser.java
+++ src/main/java/com/vladdumbrava/excel_csv_reader/service/utils/DataTypeParser.java
@@ -4,17 +4,19 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
 import com.vladdumbrava.excel_csv_reader.model.Gender;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class DataTypeParser {
 
-    public static String handleNullityInString(String s) {
+    public String handleNullityInString(String s) {
         return ((s == null || s.isBlank() ||
                 s.trim().equalsIgnoreCase("null") || s.trim().equalsIgnoreCase("n/a"))
                 ? null
                 : s.trim());
     }
 
-    public static LocalDate parseDate(String s) {
+    public LocalDate parseDate(String s) {
         try {
             return (handleNullityInString(s) == null ? null : LocalDate.parse(handleNullityInString(s)));
         } catch (DateTimeParseException e) {
@@ -22,7 +24,7 @@ public class DataTypeParser {
         }
     }
 
-    public static Gender parseGender(String s) {
+    public Gender parseGender(String s) {
         try {
             return (handleNullityInString(s) == null ? null : Gender.valueOf(handleNullityInString(s).toUpperCase()));
         }
@@ -31,7 +33,7 @@ public class DataTypeParser {
         }
     }
 
-    public static Boolean parseBoolean(String s) {
+    public Boolean parseBoolean(String s) {
         String trimmed = handleNullityInString(s);
         if (trimmed == null) return null;
         return switch (trimmed.toLowerCase()) {
